806b7d43685702d44441ba3ca1ca63def384949d
DGromek <dawidgromek.1997@gmail.com>
2020-06-05 18:39:49 +0200

    Added more state tests and behaviour tests.

--- src/test/java/edu/iis/mto/testreactor/washingmachine/WashingMachineTest.java
+++ src/test/java/edu/iis/mto/testreactor/washingmachine/WashingMachineTest.java
@@ -4,12 +4,14 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.mockito.ArgumentMatchers.anyDouble;
 import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class WashingMachineTest {
@@ -20,17 +22,19 @@ class WashingMachineTest {
     private Engine engine;
     @Mock
     private WaterPump waterPump;
+    @Mock
     private WashingMachine washingMachine;
     private ProgramConfiguration unrelevantProgramConfiguration;
     private LaundryBatch properLaundryBatch;
+    private Program defaultProgram = Program.SHORT;
 
     @BeforeEach
     void setUp() {
         washingMachine = new WashingMachine(dirtDetector, engine, waterPump);
 
         unrelevantProgramConfiguration = ProgramConfiguration.builder()
-                                                             .withProgram(Program.SHORT)
-                                                             .withSpin(false)
+                                                             .withProgram(defaultProgram)
+                                                             .withSpin(true)
                                                              .build();
 
         properLaundryBatch = LaundryBatch.builder()
@@ -39,6 +43,7 @@ class WashingMachineTest {
                                          .build();
     }
 
+    //State tests
     @Test
     void overweightLaundryBatchShouldResultInFailureAndTooHeavyErrorCode() {
         LaundryBatch outweighedLaundryBatch = LaundryBatch.builder()
@@ -60,7 +65,7 @@ class WashingMachineTest {
         LaundryStatus actual = washingMachine.start(properLaundryBatch, unrelevantProgramConfiguration);
 
         LaundryStatus expected = LaundryStatus.builder()
-                                              .withRunnedProgram(Program.SHORT)
+                                              .withRunnedProgram(defaultProgram)
                                               .withResult(Result.SUCCESS)
                                               .withErrorCode(ErrorCode.NO_ERROR)
                                               .build();
@@ -70,11 +75,12 @@ class WashingMachineTest {
 
     @Test
     void waterPumpExceptionThrownShouldResultInFailure() throws WaterPumpException {
-        doThrow(WaterPumpException.class).when(waterPump).pour(anyDouble());
+        doThrow(WaterPumpException.class).when(waterPump)
+                                         .pour(anyDouble());
 
         LaundryStatus actual = washingMachine.start(properLaundryBatch, unrelevantProgramConfiguration);
         LaundryStatus expected = LaundryStatus.builder()
-                                              .withRunnedProgram(Program.SHORT)
+                                              .withRunnedProgram(defaultProgram)
                                               .withResult(Result.FAILURE)
                                               .withErrorCode(ErrorCode.WATER_PUMP_FAILURE)
                                               .build();
@@ -84,15 +90,86 @@ class WashingMachineTest {
 
     @Test
     void engineExceptionThrownShouldResultInFailure() throws EngineException {
-        doThrow(EngineException.class).when(engine).runWashing(anyInt());
+        doThrow(EngineException.class).when(engine)
+                                      .runWashing(anyInt());
 
         LaundryStatus actual = washingMachine.start(properLaundryBatch, unrelevantProgramConfiguration);
         LaundryStatus expected = LaundryStatus.builder()
-                                              .withRunnedProgram(Program.SHORT)
+                                              .withRunnedProgram(defaultProgram)
                                               .withResult(Result.FAILURE)
                                               .withErrorCode(ErrorCode.ENGINE_FAILURE)
                                               .build();
 
         Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void properLaundryBatchWithAutodetectProgramAndDirtLevelAboveAverageShouldResultInSuccessWithLongProgram() {
+        ProgramConfiguration programConfiguration = ProgramConfiguration.builder()
+                                                                        .withProgram(Program.AUTODETECT)
+                                                                        .withSpin(true)
+                                                                        .build();
+
+        when(dirtDetector.detectDirtDegree(any())).thenReturn(new Percentage(100));
+        LaundryStatus actual = washingMachine.start(properLaundryBatch, programConfiguration);
+
+        LaundryStatus expected = LaundryStatus.builder()
+                                              .withRunnedProgram(Program.LONG)
+                                              .withResult(Result.SUCCESS)
+                                              .withErrorCode(ErrorCode.NO_ERROR)
+                                              .build();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void properLaundryBatchWithAutodetectProgramAndDirtLevelBelowAverageShouldResultInSuccessWithShortProgram() {
+        ProgramConfiguration programConfiguration = ProgramConfiguration.builder()
+                                                                        .withProgram(Program.AUTODETECT)
+                                                                        .withSpin(true)
+                                                                        .build();
+
+        when(dirtDetector.detectDirtDegree(any())).thenReturn(new Percentage(1));
+        LaundryStatus actual = washingMachine.start(properLaundryBatch, programConfiguration);
+
+        LaundryStatus expected = LaundryStatus.builder()
+                                              .withRunnedProgram(Program.MEDIUM)
+                                              .withResult(Result.SUCCESS)
+                                              .withErrorCode(ErrorCode.NO_ERROR)
+                                              .build();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    //Behaviour tests
+    @Test
+    void properLaundryBatchWithSpinShouldCallEngineAndWaterPump() throws WaterPumpException, EngineException {
+        InOrder callingOrder = inOrder(waterPump, engine);
+
+        washingMachine.start(properLaundryBatch, unrelevantProgramConfiguration);
+
+        callingOrder.verify(waterPump)
+                    .pour(properLaundryBatch.getWeightKg());
+        callingOrder.verify(engine)
+                    .runWashing(unrelevantProgramConfiguration.getProgram()
+                                                              .getTimeInMinutes());
+        callingOrder.verify(waterPump)
+                    .release();
+        callingOrder.verify(engine)
+                    .spin();
+    }
+
+    @Test
+    void properLaundryBatchWithAutodetectProgramShouldCallDirtDetector() {
+        ProgramConfiguration programConfiguration = ProgramConfiguration.builder()
+                                                                        .withProgram(Program.AUTODETECT)
+                                                                        .withSpin(true)
+                                                                        .build();
+
+        washingMachine.start(properLaundryBatch, programConfiguration);
+
+        Mockito.verify(dirtDetector)
+               .detectDirtDegree(properLaundryBatch);
+
     }
 }
